c70005d04fe5569e376dac80fe61d7cd11ddcee9
Millennium Singha <m.singha@uqconnect.edu.au>
2021-01-29 19:38:29 +0530

    Added geocoordinate watcher for the device position

--- GeoLocation/Program.cs
+++ GeoLocation/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Device.Location;
+
+/**
+ * Note: -
+ * 1. We need to have the locatin access permissions enabled in our respective device setings.
+ * 2. We also need a reference to the System.Device library for the GeoCoordinate Watcher.
+ */
 
 namespace GeoLocation
 {
@@ -10,6 +13,31 @@ namespace GeoLocation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting GeoCoordinate Wacther...");
+
+            // Use the gecoordinate watcher
+            var watcher = new GeoCoordinateWatcher();
+
+            // Determines whether the gecoordinate watcher object has been initialized or started
+            watcher.StatusChanged += (s, e) =>
+            {
+                // Here e is the event informer
+                Console.WriteLine($"GeoCoordinateWatcher:StatusChanged:{e.Status}");
+            };
+
+            // Determines the position of the device and updates accordingly
+            watcher.PositionChanged += (s, e) =>
+            {
+                Console.WriteLine($"GeoCoordinateWatcher:PositionChanged:{e.Position.Location}");
+            };
+
+            // This is the threshold with respect to initial or previous position after whic we ask for the latest position
+            watcher.MovementThreshold = 100; // meters
+
+            watcher.Start();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
